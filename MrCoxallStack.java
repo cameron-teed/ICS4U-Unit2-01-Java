b844cf1b1f4724fb6ea5b800a878513b34c2a4c9
cameron-teed <cameron.teed@mths.ca>
2021-05-17 17:32:54 +0000

    initial commit

---
+++ MrCoxallStack.java
@@ -0,0 +1,27 @@
+/*
+* This class creates an arraylist that will recieve input from a class.
+*
+* @author  Cameron Teed
+* @version 1.0
+* @since   2021-05-17
+*/
+
+// Import the ArrayList class
+import java.util.ArrayList;
+
+public class MrCoxallStack {
+
+  /**
+  * Initializes the array.
+  */
+  private ArrayList<Integer> stackAsArray = new ArrayList<Integer>();
+
+  /**
+  * This method receives the number inpputed by the user.
+  *
+  * @param pushNumber
+  */
+  public void push(final int pushNumber) {
+    stackAsArray.add(pushNumber);
+  }
+}
---
+++ StackForm.java
@@ -0,0 +1,52 @@
+/*
+* This program pushes a user input to a program with a class containing an
+* arraylist. Then prints out what number you pushed to the stack.
+*
+* @author  Cameron Teed
+* @version 1.0
+* @since   2021-05-17
+*/
+
+// Import the Scanner class
+import java.util.Scanner;
+
+public final class StackForm {
+
+  private StackForm() {
+    // Prevent instantiation
+    // Optional: throw an exception e.g. AssertionError
+    // if this ever *is* called
+     throw new IllegalStateException("Cannot be instantiated");
+  }
+
+  /**
+  * This program recieves input and adds values onto the stack.
+  *
+  * @param args
+  */
+  public static void main(final String[] args) {
+
+    // create scanner object
+    Scanner userInput = new Scanner(System.in);
+
+    // ensures that user inputs only integer values
+    try {
+      // receive user input
+      System.out.println("Enter an integer to push onto the stack: ");
+      int numberChosen = userInput.nextInt();
+      System.out.println();
+
+      // references MrCoxallStack file
+      MrCoxallStack clasStack = new MrCoxallStack();
+
+      // Pushes user input into arraylist in MrCoxallStack
+      clasStack.push(numberChosen);
+
+      // Retrieves value from arraylist in MrCoxallStack
+      System.out.println("Pushed: " + numberChosen);
+
+    } catch (Exception e) {
+      System.out.println("Invalid Input");
+    }
+  }
+}
